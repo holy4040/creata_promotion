2f977667dce1683d882d4262e99fe9f324aa0d96
holy4040 <zhaobinqu@hotmail.com>
2022-06-08 15:03:04 +1000

    chore: add nginx docker

---
+++ promotion/celery.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+import os
+
+from celery import Celery
+from promotion.settings import base
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "real_estate.settings.development")
+
+app = Celery("promotion")
+
+app.config_from_object("promotion.settings.development", namespace="CELERY"),
+
+app.autodiscover_tasks(lambda: base.INSTALLED_APPS)
--- scripts/initial_data.py
+++ scripts/initial_data.py
@@ -6,4 +6,6 @@ from apps.promotioncode.serializers import (PromotionCodeCreateSerializer,
 def run():
     code_list = ["ZVLJCYTG", "AC4ICZFM", "2PKFJHPY", "GF2GY8ZF", "XH4Z5946"]
     for c in code_list:
-        PromotionCode.objects.create(promo_code=c)
+        try:
+            PromotionCode.objects.create(promo_code=c)
+        except:pass
